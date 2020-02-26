5bdba6efc2c3b75e2c7eddff56dcb9c953f53d3c
Voggastur <v3x0r_x014@hotmail.com>
2020-02-26 17:48:58 +0000

    changed calc.js

--- spec/calcSpec2.js
+++ spec/calcSpec2.js
@@ -1,9 +1,9 @@
-describe("bartender", function() {
-    describe("whatCanIDrink tests", function() {
-        it("should return message", function() {
-            expect(whatCanIDrink(13)).toBe(42);
+describe("Calculator", function() {
+    describe("Addition tests", function() {
+        it("should return 42", function() {
+            expect(addition(20, 22)).toBe(42);
         });
-        it("should return message", function() {
+        it("should return 26", function() {
             expect(addition(7, 19)).toBe(26);
         });
         it("should return an error if we don't supply two numbers", function() {
