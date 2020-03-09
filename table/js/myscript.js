5a685c01257dbeb1a502f3a92f22b8c482fb6f2d
Voggastur <v3x0r_x014@hotmail.com>
2020-03-09 18:43:35 +0000

    added heading and uls to table.html with js and css stylesheet

--- table/js/myscript.js
+++ table/js/myscript.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
     $("h2").addClass("underline");
     $("ul").addClass("border");
-    $("h2").removeClass("underline");
-    $("ul").removeClass("border");
+    $("ul").css("list-style", "armenian");
+    $("li").css("color", "darkblue");
 });
